5867cff32380c94ffd33f0b2819e7a32d9118a03
Davide64-dev <davidpcu@hotmail.com>
2024-04-29 16:44:08 +0100

    added rectangle

--- proj/src/main.c
+++ proj/src/main.c
@@ -1,14 +1,12 @@
 #include <lcom/lcf.h>
-#include <lcom/lcf.h>
 #include <lcom/timer.h>
 
-#include <stdbool.h>
-#include <stdint.h>
 #include "drivers/gpu.h"
 #include "drivers/keyboard.h"
+#include <stdbool.h>
+#include <stdint.h>
 
 extern uint8_t scancode;
-
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -34,17 +32,23 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+int(proj_main_loop)() {
 
-
-int (proj_main_loop)(){
-
-  if (timer_set_frequency(0, 60) != 0) return 1;
+  if (timer_set_frequency(0, 60) != 0)
+    return 1;
   uint8_t irq_set_timer;
   uint8_t irq_set_keyboard;
-  
-  if (timer_subscribe_int(&irq_set_timer) != 0) return 1;
-  if (kbd_subscribe_int(&irq_set_keyboard) != 0) return 1;
-  if (vg_enter(0x105) != 0) return 1;
+
+  if (timer_subscribe_int(&irq_set_timer) != 0)
+    return 1;
+  if (kbd_subscribe_int(&irq_set_keyboard) != 0)
+    return 1;
+  if (vg_enter(0x105) != 0)
+    return 1;
+
+  if (create_vram_buffer(0x105) != 0) return 1;
+
+  if (draw_rectangle(0x105, 100, 100, 100, 100, 0x20) != 0) return 1;
 
   int ipc_status;
   int r;
@@ -52,39 +56,41 @@ int (proj_main_loop)(){
   int oscillations = 0;
   message msg;
 
-  while(scancode != ESC_BREAK) {
+  while (scancode != ESC_BREAK) {
 
-      if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
-          continue;
-      }
-      if (is_ipc_notify(ipc_status)) { 
-          switch (_ENDPOINT_P(msg.m_source)) {
-              case HARDWARE:		
-                  if (msg.m_notify.interrupts & irq_set_timer) {
-                      oscillations++;
-                      if (oscillations == 60) {
-                        timer_print_elapsed_time();
-                        oscillations = 0;
-                        count++;  
-                      }
-                  }
-
-                  if (msg.m_notify.interrupts & irq_set_keyboard){
-                    kbc_ih();
-                  }
-
-                  break;
-              default:
-                  break;
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & irq_set_timer) {
+            oscillations++;
+            if (oscillations == 60) {
+              timer_print_elapsed_time();
+              oscillations = 0;
+              count++;
+            }
           }
-      } else {
 
+          if (msg.m_notify.interrupts & irq_set_keyboard) {
+            kbc_ih();
+          }
+
+          break;
+        default:
+          break;
       }
+    }
+    else {
+    }
   }
-    printf("Hello, World!\n");
-    if (timer_unsubscribe_int() != 0) return 1;
-    if (kbd_unsubscribe_int() != 0) return 1;
-    if(vg_exit() != 0) return 1;
-    return 0;
+  printf("Hello, World!\n");
+  if (timer_unsubscribe_int() != 0)
+    return 1;
+  if (kbd_unsubscribe_int() != 0)
+    return 1;
+  if (vg_exit() != 0)
+    return 1;
+  return 0;
 }
-

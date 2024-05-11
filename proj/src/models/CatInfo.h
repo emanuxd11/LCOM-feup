cc9c3d8e374c7bedda5fa49dacce9ddbea9815dd
edo:3 <eduardofbaltazar@gmail.com>
2024-05-11 13:28:53 +0100

    add CatInfo.h and PlayerInfo.h

---
+++ proj/src/models/CatInfo.h
@@ -0,0 +1,15 @@
+
+
+enum CatColor {
+    ORANGE,
+    WHITE,
+    BLACK,
+    GRAY,
+    BROWN
+};
+
+typedef struct {
+    CatColor color;
+    bool isPet;
+    bool isAngry;
+} CatInfo;
---
+++ proj/src/models/PlayerInfo.h
@@ -0,0 +1,5 @@
+
+
+typedef struct {
+    bool isPetting;
+} PlayerInfo;

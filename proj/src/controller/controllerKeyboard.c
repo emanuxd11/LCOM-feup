4189bb5da9b94cd76f4bbf382a3ac37fa01fc483
edo:3 <eduardofbaltazar@gmail.com>
2024-05-24 18:46:50 +0100

    controllerKeyboard now updates key states; still need to transfer game control functions elsewhere; not runnable

--- proj/src/controller/controllerKeyboard.c
+++ proj/src/controller/controllerKeyboard.c
@@ -1,6 +1,62 @@
 #include "controllerKeyboard.h"
 
 
+// check when held down
+bool wIsDown = false;
+bool aIsDown = false;
+bool sIsDown = false;
+bool dIsDown = false;
+
+// check single press (true on break code; to be turned back to false by reader function)
+bool escWasPressed = false;
+
+int update_keys(uint8_t scan_code) {
+
+    switch (scan_code)
+    {
+
+    //held down
+    case W_MAKE:
+        wIsDown = true;
+        break;
+    case W_BREAK:
+        wIsDown = false;
+        break;
+
+    case A_MAKE:
+        aIsDown = true;
+        break;
+    case A_BREAK:
+        aIsDown = false;
+        break;
+
+    case S_MAKE:
+        sIsDown = true;
+        break;
+    case S_BREAK:
+        sIsDown = false;
+        break;
+    
+    case D_MAKE:
+        dIsDown = true;
+        break;
+    case D_BREAK:
+        dIsDown = false;
+        break;
+
+    //single press
+    case ESC_BREAK:
+        escWasPressed = true;
+        break;
+    
+    default:
+        break;
+    }
+
+
+}
+
+
 int control_game(Game *game, uint8_t scan_code) {
     if (scan_code == ESC_BREAK) {
         switch(game->state) {

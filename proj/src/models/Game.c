dbc8ba33560712df965251fa6daa4ff2ed3f1c19
Emanuel Maia <tavanomanu@gmail.com>
2024-05-30 16:10:27 +0100

    merge

--- proj/src/models/Game.c
+++ proj/src/models/Game.c
@@ -11,7 +11,7 @@ Datetime datetime;
 
 Game *createNewGame() {
   Game *game = (Game *) malloc(sizeof(Game));
-  game->state = GAME_STATE;
+  game->state = MENU_STATE;
   game->room = newRoom();
   return game;
 }

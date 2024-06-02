e46fcc4259a32a7cab72a1ed2ad6bc5d631f1641
Joana Noites <joanalune@gmail.com>
2024-06-02 17:54:33 +0100

    Room.h comments

--- proj/src/models/PlayerInfo.h
+++ proj/src/models/PlayerInfo.h
@@ -1,6 +1,14 @@
+/**
+ * @file PlayerInfo.h
+ * @brief This file contains a small struct with player info
+ * @date 2024-06-02
+ */
 #ifndef _PLAYERINFO_H_
 #define _PLAYERINFO_H_
-
+/**
+ * @brief PlayerInfo struct
+ * 
+ */
 typedef struct {
     bool isPetting;
 } PlayerInfo;

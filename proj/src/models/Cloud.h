1cca183be4ef01681a6ecfa38c645f97fdb88f04
Joana Noites <joanalune@gmail.com>
2024-06-02 16:57:29 +0100

    Cloud and datetime comments

--- proj/src/models/Cloud.h
+++ proj/src/models/Cloud.h
@@ -1,3 +1,8 @@
+/**
+ * @file Cloud.h
+ * @brief tHis file contains information about the clouds drawn in the game
+ * @date 2024-06-02
+ */
 #ifndef _CLOUD_H_
 #define _CLOUD_H_
 
@@ -5,7 +10,10 @@
 #include <stdlib.h>
 #include "../utils/aux_structs/Position.h"
 
-
+/**
+ * @brief Cloud information
+ * 
+ */
 typedef struct {
   double x, y;
   double velocity;
